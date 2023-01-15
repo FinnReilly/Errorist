fed1527803d35042b6fefba5103427a96a2c8f33
Finn Reilly <finn.reilly@mountainwarehouse.com>
2023-01-15 23:37:49 +0000

    Extension methods for use in asp net

---
+++ Errorist/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Errorist.Middleware;
+using Microsoft.AspNetCore.Builder;
+
+namespace Errorist.Extensions
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder UseErrorConfiguration<TOutput>(this IApplicationBuilder builder)
+            where TOutput : class, new()
+            => builder.UseMiddleware<ExceptionHandlingMiddleware<TOutput>>();
+    }
+}
---
+++ Errorist/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Errorist.Implementations;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Errorist.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddErrorConfiguration(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IExceptionFormattingService<>), typeof(ExceptionFormattingService<>));
+            services.AddSingleton(typeof(IHttpContextConfigurator<>), typeof(DefaultContextConfigurator<>));
+
+            return services;
+        }
+    }
+}
