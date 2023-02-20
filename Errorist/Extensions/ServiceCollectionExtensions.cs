287583949b72cd77c312dbc1a75e79da41de937b
Finn Reilly <finn.reilly@mountainwarehouse.com>
2023-02-20 00:18:41 +0000

    Debug DI Issue from interface segregation

--- Errorist/Extensions/ServiceCollectionExtensions.cs
+++ Errorist/Extensions/ServiceCollectionExtensions.cs
@@ -8,8 +8,9 @@ namespace Errorist.Extensions
         public static IServiceCollection AddErrorConfiguration<TOutput>(this IServiceCollection services)
             where TOutput : class, new()
         {
-            services.AddScoped<IExceptionOutputConfigurator<TOutput>, ExceptionFormattingService<TOutput>>();
-            services.AddScoped<IExceptionScopeProvider<TOutput>, ExceptionFormattingService<TOutput>>();
+            services.AddScoped<ExceptionFormattingService<TOutput>>();
+            services.AddScoped<IExceptionOutputConfigurator<TOutput>, ExceptionFormattingService<TOutput>>(x => x.GetRequiredService<ExceptionFormattingService<TOutput>>());
+            services.AddScoped<IExceptionScopeProvider<TOutput>, ExceptionFormattingService<TOutput>>(x => x.GetRequiredService<ExceptionFormattingService<TOutput>>());
             services.AddSingleton<IHttpContextConfigurator<TOutput>, DefaultContextConfigurator<TOutput>>();
             services.AddSingleton<IExceptionFormattingGlobalScope<TOutput>, ExceptionFormattingGlobalScope<TOutput>>();
             services.AddSingleton<IConfigurationBuilderFactory, ConfigurationBuilderFactory>();
