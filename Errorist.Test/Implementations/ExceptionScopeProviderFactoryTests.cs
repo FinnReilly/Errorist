acc2e875b4cf36786e24bb8b9be20467ae845c81
Finn Reilly <finn.reilly@mountainwarehouse.com>
2023-03-07 22:08:11 +0000

    unit test for ExceptionScopeProviderFactory

---
+++ Errorist.Test/Implementations/ExceptionScopeProviderFactoryTests.cs
@@ -0,0 +1,58 @@
+﻿using Errorist.Implementations;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Errorist.Test.Implementations
+{
+    [TestFixture]
+    public class ExceptionScopeProviderFactoryTests
+    {
+        private Mock<IHttpContextAccessor> _contextAccessor;
+
+        private ExceptionScopeProviderFactory<TestOutputType> _sut;
+
+        private DefaultHttpContext _returnedContext;
+        private Mock<IServiceProvider> _requestServiceProvider;
+        private Mock<IExceptionScopeProvider<TestOutputType>> _returnedScopeProvider;
+
+        [SetUp]
+        public void Setup()
+        {
+            _contextAccessor = new Mock<IHttpContextAccessor>();
+
+            _sut = new ExceptionScopeProviderFactory<TestOutputType>(_contextAccessor.Object);
+
+            _requestServiceProvider = new Mock<IServiceProvider>();
+            _returnedScopeProvider = new Mock<IExceptionScopeProvider<TestOutputType>>();
+            _returnedContext = new DefaultHttpContext 
+            {
+                RequestServices = _requestServiceProvider.Object,
+            };
+
+            _requestServiceProvider
+                .Setup(s => s.GetService(typeof(IExceptionScopeProvider<TestOutputType>)))
+                .Returns(_returnedScopeProvider.Object)
+                .Verifiable();
+            _contextAccessor.Setup(s => s.HttpContext)
+                .Returns(_returnedContext)
+                .Verifiable();
+        }
+
+        [Test]
+        public void CurrentProvider_ReturnsScopedInstanceFromRequestServices()
+        {
+            // Act
+            var actualScopeProvider = _sut.CurrentProvider;
+
+            // Assert
+            _contextAccessor.Verify();
+            _requestServiceProvider.Verify();
+            Assert.That(actualScopeProvider, Is.EqualTo(_returnedScopeProvider.Object));
+        }
+    }
+}
